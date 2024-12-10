5dfcd942e1eb307f33a1344ddaecfbad6e2ada74
kestsaurav21 <kestsaurav21@outlook.com>
2024-12-11 02:29:56 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import './App.css'
 import Category from './pages/Category'
 

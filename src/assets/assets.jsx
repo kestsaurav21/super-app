dff3c07ef3ee7ed4a7c304032496356773e6893d
kestsaurav21 <kestsaurav21@outlook.com>
2024-12-10 23:19:47 +0530

    assets

--- src/assets/assets.jsx
+++ src/assets/assets.jsx
@@ -1,5 +1,23 @@
 import banner from './banner.png'
+import action from './action.png' 
+import drama from './drama.png'
+import fantasy from './fantasy.png'
+import horror from './horror.png'
+import fiction from './fiction.png'
+import romance from './romance.png'
+import thriller from './thriller.png'
+import western from './western.png'
+import music from './music.png'
 
 export default {
-  banner
+  banner,
+  action,
+  drama,
+  fantasy,
+  horror,
+  fiction,
+  romance,
+  thriller,
+  western,
+  music
 }

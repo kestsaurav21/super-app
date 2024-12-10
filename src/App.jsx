5544a8b23bffa972db9c6146ba2510226079feda
kestsaurav21 <kestsaurav21@outlook.com>
2024-12-11 01:25:56 +0530

    update: route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/home'
 import './App.css'
+import Category from './pages/Category'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/category" element={<Category />} />
       </Routes>
    </BrowserRouter>
    </>

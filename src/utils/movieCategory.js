eae7fc4149153616b4d6e0e8349e2249edfdbe7f
kestsaurav21 <kestsaurav21@outlook.com>
2024-12-10 23:20:05 +0530

    mock data

---
+++ src/utils/movieCategory.js
@@ -0,0 +1,70 @@
+import assets from "../assets/assets";
+
+const movieCategory = [
+    {
+        id: 1,
+        value: "action",
+        label: "Action",
+        image: assets.action,
+        backgroundColor: "#FF5209"
+    },
+    {
+        id: 2,
+        value: "drama",
+        label: "Drama",
+        image: assets.drama,
+        backgroundColor: "#D7A4FF"
+    },
+    {
+        id: 3,
+        value: "romance",
+        label: "Romance",
+        image: assets.romance,
+        backgroundColor: "#148A08"
+    },
+    {
+        id: 4,
+        value: "thriller",
+        label: "Thriller",
+        image: assets.thriller,
+        backgroundColor: "#84C2FF"
+    },
+    {
+        id: 5,
+        value: "western",
+        label: "Western",
+        image: assets.western,
+        backgroundColor: "#902500"
+    },
+    {
+        id: 6,
+        value: "horror",
+        label: "Horror",
+        image: assets.horror,
+        backgroundColor: "#7358FF"
+    },
+    {
+        id: 7,
+        value: "fantasy",
+        label: "Fantasy",
+        image: assets.fantasy,
+        backgroundColor: "#FF4ADE"
+    },
+
+    {
+        id: 8,
+        value: "music",
+        label: "Music",
+        image: assets.music,
+        backgroundColor: "#E61E32"
+    },
+    {
+        id: 9,
+        value: "fiction",
+        label: "Fiction",
+        image: assets.fiction,
+        backgroundColor: "#6CD061"
+    }
+]
+
+export default movieCategory;       
